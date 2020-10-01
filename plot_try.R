16f0563aea22672d51abb115dca4c5138fe578b6
Shashank <shashankt693@gmail.com>
2020-10-01 16:29:40 +0530

    Final code

--- plot_try.R
+++ plot_try.R
@@ -1,69 +1,51 @@
-bed <- read.csv("~/Project/Data/NiGa/spliaa.bedpe",header=FALSE,sep="\t",stringsAsFactors=FALSE, quote="")
+#import libraries
 library(doMC)
 library(ggplot2)
 registerDoMC(cores = 4)
 library(data.table)
 library(dplyr)
 library(plyr)
+#Read file
 yyy = fread("~/Project/Data/NiGa/uniq.TGACCA_D13_3.bedpe",header=FALSE,sep = "\t",stringsAsFactors = FALSE,quote = "")
 colnames(yyy) <- c('rnachrom','rnachromStart','rnachromEnd','dnachrom','dnachromStart','dnachromEnd','cigar','ignore','rnaStrand','dnaStrand','rnQual','dnaQua','rnaID')
+
+#Make length colums
 yyy$len <- abs((yyy$dnachromStart+yyy$dnachromEnd)/2-(yyy$rnachromStart+yyy$rnachromEnd)/2)
 mean(yyy$len)
+
+#Remove trans inetractions
 yyy$match <- NULL
 yyy[yyy$rnachrom == yyy$dnachrom,"match"] <-"1"
 yyy[yyy$rnachrom != yyy$dnachrom,"match"] <-"0"
 vc <- '1'
 schr <- yyy[is.element(yyy$match,vc),]
-table(schr$len)
-barplot(table(schr$len))
-ggplot(schr,aes(x = len))+
-  geom_histogram(aes(y = stat(count)/sum(count)))+
-  scale_y_continuous(labels = scales::percent)
+#########
 
-lsd <- c(unique(schr$rnaID))
-lsd[2]
-
-schr[order(schr$rnaID,decreasing = TRUE)]
-sdf <- daply(schr,)
+#Sort the file
 kls <- split(schr,schr$rnaID)
-gir <- NULL
-for (i in 1:length(kls)){
-  gir[i] <- length(kls[[i]]$rnachrom)
-}
-#########################
-pdf(file="Plots.pdf")
-for (i in 1:length(kls)){
-  if (length(kls[[i]]$rnachrom)>100){
-    ggplot(kls[[i]],aes(x = len))+
-      geom_histogram(aes(y = stat(count)/sum(count)))+
-      scale_y_continuous(labels = scales::percent)
-  }
-}
-dev.off()
+
+######################### Get the plots
 ###################
+setwd("~/Project/Plots/D13_3/")
+
 plot_list = list()
 for (i in 1:length(kls)){
   if (length(kls[[i]]$rnachrom)>100){
     p = ggplot(kls[[i]],aes(x = len))+
       geom_histogram(aes(y = stat(count)/sum(count)))+
-      scale_y_continuous(labels = scales::percent)
+      scale_y_continuous(labels = scales::percent)+
+      ggtitle(kls[[i]]$rnaID[1])
+    plot_list[[i]] = p
   } else {
     p = ggplot(kls[[i]],aes(x = len))+
       geom_freqpoly(aes(y = stat(count)/sum(count)))+
-      scale_y_continuous(labels = scales::percent)
-  }
-  plot_list[[i]] = p
-}
-tt <- NULL
-for (i in 1:length(gir)){
-  if (gir[i]>100){
-    tt[i] <- 1
+      scale_y_continuous(labels = scales::percent)+
+      ggtitle(kls[[i]]$rnaID[1])
+    plot_list[[i]] = p
   }
 }
-count(tt)
-class(gir)
 for (i in 1:length(kls)){
-  file_name = paste("iris_plot_", i, ".tiff", sep="")
+  file_name = paste("distance_plot_", kls[[i]]$rnaID[1], ".tiff", sep="")
   tiff(file_name)
   print(plot_list[[i]])
   dev.off()
@@ -73,42 +55,9 @@ for (i in 1:length(kls)){
   print(plot_list[[i]])
 }
 dev.off
-print(plot_list[[3]])
+
 
 #######
 
 
-ggplot(kls[[1]],aes(x = len))+
-  geom_freqpoly(aes(y = stat(count)/sum(count)))+
-  scale_y_continuous(labels = scales::percent)
 
-plot(kls[[2]]$len)
-frequency()
-for (i in 1:length(lsd)){
-  f_df[[i]] <- filter(schr,rnaID == lsd[i] )
-  ggplot(f_df[[i]],aes(x = len))+
-    geom_histogram(aes(y = stat(count)/sum(count)))+
-    scale_y_continuous(labels = scales::percent)
-}
-#---------------------------------------------#
-
-plot_list = list()
-for (i in 1:length(kls)){
-  if (length(kls[[i]]$rnachrom)>100){
-    p = ggplot(kls[[i]],aes(x = len))+
-      geom_histogram(aes(y = stat(count)/sum(count)))+
-      scale_y_continuous(labels = scales::percent)
-  }
-  plot_list[[i]] = p
-}
-for (i in 1:3051){
-  file_name = paste("iris_plot_", i, ".tiff", sep="")
-  tiff(file_name)
-  print(plot_list[[i]])
-  dev.off()
-}
-pdf("plots.pdf")
-for (i in 1:3051){
-  print(plot_list[[i]])
-}
-dev.off
